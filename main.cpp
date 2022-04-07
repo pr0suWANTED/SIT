7fedf9ecc99128f0017a23950ea3f3e412e02769
pr0suWANTED <prosuwanted@gmail.com>
2022-04-07 13:09:54 +0400

    Rewrote the fib function iteratively

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int fib(int n) {
-	if (n <= 1) return 0;
-	if (n == 2) return 1;
-	return fib(n - 1) + fib(n - 2);
+	int p1 = 0;
+	int p2 = 1;
+	int s = 0;
+	for (int i = 0; i < n - 1; i++) {
+		s = p2 + p1;
+		p2 = p1;
+		p1 = s;
+		cout << s << endl;
+	}
+	return s;
 }
 
 int main() {
 	int n;
 	cout << "Please enter a number: ";
 	cin >> n;
-	cout << "fib(" << n << "): " << fib(n) << endl;
+	fib(n);
 	return 0;
 }
