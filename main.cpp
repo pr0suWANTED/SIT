055d18eef8358074d609f595ce905931ac163b88
pr0suWANTED <prosuwanted@gmail.com>
2022-04-07 12:44:54 +0400

    Testing with Fibonacci

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n) {
+	if (n <= 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
-	cout << "Hello World!" << endl;
+	int n;
+	cout << "Please enter a number: ";
+	cin >> n;
+	cout << "fib(" << n << "): " << fib(n) << endl;
 	return 0;
 }
